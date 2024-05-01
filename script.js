087e3b7ae563ff0cd19465202b35f7d1f0237748
elizabethtrykin <72474571+elizabethtrykin@users.noreply.github.com>
2024-05-01 11:23:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,142 @@
+const categories = {
+    innerWisdom: [
+        "What’s the last thing you learned about yourself that surprised you?",
+        "What’s the lesson that took you the longest to learn?",
+        "What was one experience that caused you to re-evaluate your beliefs?",
+        "What was your biggest failure this year? What did you learn from it?",
+        "Whose love did you crave the most growing up? Who did you need to be to receive that love?",
+        "Who inspires you to be better? When was the last time you felt inspired?",
+        "Who or what is the greatest love of your life? What did they teach you?",
+        "What activities give you the most pleasure and joy? What do you think that says about you?",
+        "Who do people see you as or say that you are that is just not accurate?",
+        "What’s one moment you felt you were transitioning from a girl to a woman/boy to a man?",
+        "What’s the best thing you inherited from each of your parents? The worst?",
+        "What are the ways in which you have grown or changed over the last five years?",
+        "What have you learned about yourself through your relationship with food? Do you see it as a friend or an enemy?",
+        "How has your definition of success changed as you’ve grown older and wiser?",
+        "In what ways have you learned to people-please or be the “good” girl in your life?",
+        "What’s one lesson your parents taught you that you would to pass on to your children or the next generation?",
+        "How has your experience with religion shaped your views on femininity and womanhood?",
+        "What did your parents’ marriage teach you about love and partnerships?",
+        "What’s important to you? Share three values you live by.",
+        "What invaluable lesson did you learn from your worst relationship?",
+        "At this point in your life what’s your experience of being a woman?",
+        "What have you learned about your body and sexuality?",
+        "What have you learned from your relationships with women, about being a woman?",
+        "How has your definition of beauty evolved over the years? Do you feel that you are growing more or less beautiful?",
+        "What are your core beliefs about your ability to be powerful?"
+    ],
+    givingBirth: [
+        "When was the last time you created something out of nothing?",
+        "What do you think your work says about your character?",
+        "How would you define creativity? What does being creative mean to you?",
+        "Do people regard you as a creative person? Do you agree?",
+        "In your experience, how does creativity relate to being a woman?",
+        "What would you create if you know you could not fail?",
+        "What habit do you have that gets in the way of your creativity? What surprising benefit does it bring?",
+        "What would you like to create in the next twelve months?",
+        "We birth projects, ideas, art, goals, relationships, babies and more. How do you feel about the subject of birth?",
+        "Have any of your friends/family/partners triggered doubts in you about your abilities to create?",
+        "What qualities are you most proud of in yourself? Share a story where you applied that quality",
+        "Do your beliefs limit or support your creative expansion?",
+        "Did your parents support your creativity as a child? In what ways did they show this?",
+        "Who has been the biggest supporter of your creative self-worth?",
+        "In what ways do you nurture yourself and your creative work?",
+        "If you could change one aspect of our society through your work, what would that be?",
+        "What percentage of your time is spent helping others vs pursuing your own desires?",
+        "Do you ever dream of starting a business? If yes, what would that be?",
+        "Do you enjoy sharing your work with others or do you prefer to go unnoticed? Why?",
+        "When it comes to your work, what words of encouragement do you most want to hear?",
+        "Who would you most like to impact with your work?",
+        "Looking at what you have accomplished in the past, would you change anything today? Why or why not?",
+        "How do other peoples opinions or reactions affect what you create?",
+        "Of all the things you have created, what are you most proud of?",
+        "When you think about the things you have created, what’s something you disliked but others loved?"
+    ],
+    theShadow: [
+        "What’s your greatest fear about getting older?",
+        "What’s the story you repeat that keeps you from having the life that you want? Who would be without that story?",
+        "Who are you jealous or envious of?",
+        "Describe one experience you wish you never had?",
+        "What’s something you would like to be forgiven for?",
+        "What do you blame your partner for? Is it fair?",
+        "Who In your life have you been judging harshly?",
+        "What are you inclined to lie about? What was the last lie you told?",
+        "What triggers you? What do yo think is the wound behind the trigger?",
+        "What’s the pain in you that you wish you could heal?",
+        "In what ways have you learned to mistrust yourself? In what ways have you learned to mistrust others?",
+        "Which of your insecurities create the most struggle in your relationships?",
+        "Looking back, who was the intimate partner you treated most unfairly?",
+        "Who in your life are you trying to save or convince?",
+        "What are you trying to control or bend to your will?",
+        "When do you feel the least powerful? What do you do when you feel this way?",
+        "What are some limiting beliefs about yourself that are holding you back?",
+        "What are you tolerating, or pretending to like, out of duty or obligation?",
+        "What did you say to yourself the last time things didn’t work out?",
+        "Are you more likely to avoid or create conflict? Share a story where you’ve avoided or created conflict",
+        "What are you scared of losing?",
+        "What daily habits do you need to let go of?",
+        "When was the last time you were disappointed by someone?",
+        "What expectations did you have that was not met?",
+        "What angers you most? What might others interpret about you from this?",
+        "What makes you difficult to live with?"
+    ],
+    desire: [
+        "If you had the money and resources what would be your dream job?",
+        "When you experience stress or overwhelmed how do you like to be supported?",
+        "How much money would you want to make to feel totally safe?",
+        "How would you want your life to unfold in the next ten years? What would your love life, career or family look like?",
+        "If you could choose to never work again, how would you spend your days?",
+        "What would a perfect Sunday look like for you? How would the day unfold from morning to dusk?",
+        "What would an ideal partner look like for you? Describe three character traits",
+        "In your wildest fantasies, what do you allow yourself to have?",
+        "If you could wake up tomorrow and instantly be an expert on a subject, what would that be?",
+        "What would you like to change about the way you were raised?",
+        "What did you yearn for growing up that your parents did not give you?",
+        "What is an unreasonable desire you’re sure you cannot have?",
+        "What’s something you want to say to a loved one? What stops you from doing so?",
+        "If you have a platform that could reach a million people, what would you want to say? What would your message be?",
+        "If you could ask a sage any question, what would you want to know?",
+        "What mistakes would you avoid in future relationships?",
+        "What do you want to be remembered for but have not done yet?",
+        "Who are you jealous or envious of? Why?",
+        "Describe one thing you want more of in your life.",
+        "What would your ideal home look like? Describe the location, exterior and interior.",
+        "What are three things you want to give this year? What are three things you would like to receive this year?",
+        "What’s something you would like to learn this year?",
+        "What are the top three things on your bucket list? Why are they important to you?",
+        "If you weren’t afraid of what people would think, say or do, what would you do?",
+        "What’s on thing you would like to add to your career that would make it more fulfilling?",
+        "Describe one experience you wish to have in the future."
+    ]
+};
+
+let currentCategory = '';
+let currentQuestionIndex = 0;
+
+function selectCategory(category) {
+    currentCategory = category;
+    currentQuestionIndex = 0;
+    document.getElementById('category-container').style.display = 'none';
+    document.getElementById('question-container').style.display = 'flex';
+    showNextQuestion();
+}
+
+function showNextQuestion() {
+    const questions = categories[currentCategory];
+    const randomIndex = Math.floor(Math.random() * questions.length); // Generate a random index
+    const questionNumber = randomIndex + 1; // Adjust index to be human-readable (starting from 1)
+    const questionText = questionNumber + ". " + questions[randomIndex]; // Get random question with number
+    document.getElementById('question').innerText = questionText; // Display the question
+}
+
+
+function goBack(event) {
+    event.stopPropagation(); // Prevent the question change when clicking 'Back'
+    document.getElementById('question-container').style.display = 'none';
+    document.getElementById('category-container').style.display = 'flex';
+}
+
+window.onload = function() {
+    document.getElementById('category-container').style.display = 'flex';
+};
